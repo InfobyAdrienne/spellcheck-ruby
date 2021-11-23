c64be09a8dc6b2f75974bd160e1732c7e2608d12
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-11-23 13:59:54 +0000

    passing test for letter cases

--- lib/spellcheck.rb
+++ lib/spellcheck.rb
@@ -5,13 +5,13 @@ def sentence_to_check(sentence)
   new_sentence = sentence.split(" ")
   
   corrected_sentence = new_sentence.map { |word| 
-    if dictionary.include?(word)
+    if dictionary.include?(word.downcase)
       word
     else
       "~#{word}~" 
     end
   }
 
-return corrected_sentence.join(" ")
+return corrected_sentence.join(" ").downcase
 
 end
--- spec/spellcheck_spec.rb
+++ spec/spellcheck_spec.rb
@@ -5,11 +5,15 @@ describe 'highlight the error in a spelt word' do
   expect(sentence_to_check("spelt")).to eq "spelt"
  end 
  
-  it 'should highlight the error in a mispelled word' do 
+  it 'should highlight the error in a misspelled word' do 
   expect(sentence_to_check("spnelt")).to eq "~spnelt~"
  end
 
- it 'should highlight the error in a mispelled word within a sentence' do 
+ it 'should return the word if it is spelled correctly, despite letter cases' do 
+  expect(sentence_to_check("SPELT")).to eq "spelt"
+ end 
+
+ it 'should highlight the error in a misspelled word within a sentence' do 
   expect(sentence_to_check("these words are spnelt")).to eq "these words are ~spnelt~"
  end 
 
