a2a1a5025c75903c46a992af4eef940a220b2117
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-11-23 13:39:07 +0000

    passing three tests

--- lib/spellcheck.rb
+++ lib/spellcheck.rb
@@ -1,3 +1,17 @@
 def sentence_to_check(sentence)
-  return "~#{sentence}~"
+
+  dictionary = ["these", "words", "are", "spelt", "correctly"]
+
+  new_sentence = sentence.split(" ")
+  
+  corrected_sentence = new_sentence.map { |word| 
+    if dictionary.include?(word)
+      word
+    else
+      "~#{word}~" 
+    end
+  }
+
+return corrected_sentence.join(" ")
+
 end
--- spec/spellcheck_spec.rb
+++ spec/spellcheck_spec.rb
@@ -1,12 +1,16 @@
 require 'spellcheck'
 
-describe 'highlight teh error in a spelt word' do 
- it 'should highlight the error in a mispelled word' do 
+describe 'highlight the error in a spelt word' do 
+ it 'should return the word if it is spelled correctly' do 
+  expect(sentence_to_check("spelt")).to eq "spelt"
+ end 
+ 
+  it 'should highlight the error in a mispelled word' do 
   expect(sentence_to_check("spnelt")).to eq "~spnelt~"
  end
 
-#  it 'should highlight the error in a mispelled word within a sentence' do 
-#   expect(sentence_to_check("These words are spnelt")).to eq "These words are ~spnelt~"
-#  end 
+ it 'should highlight the error in a mispelled word within a sentence' do 
+  expect(sentence_to_check("these words are spnelt")).to eq "these words are ~spnelt~"
+ end 
 
 end 
